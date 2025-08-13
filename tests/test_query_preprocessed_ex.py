100d93c9652cc881a81519bbe3cb7311f0000dd4
Loris Dematini <loris.dematini@yahoo.fr>
2025-08-13 16:52:00 +0200

    pyproject

--- tests/test_query_preprocessed_ex.py
+++ tests/test_query_preprocessed_ex.py
@@ -7,5 +7,6 @@ from search_engine.core.processed_exact import preprocess_query_ex
     ("well-being", "wellbeing"),
     ("OpenAI", "openai"),
 ])
+
 def test_preprocess_query_ex(input_text, expected):
     assert preprocess_query_ex(input_text) == expected
